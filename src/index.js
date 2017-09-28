2f078a30f1134c1d8ded9cc10c67827739a20582
Helen Trapeznikova <31996958+HelenTR7@users.noreply.github.com>
2017-09-28 23:40:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports =function multiply(first, second) {
-  //var first_mass=[];
+
   var long_str, short_str;
   if(first.length>=second.length)
   {
@@ -18,35 +18,96 @@ module.exports =function multiply(first, second) {
     newmass[i]=0;
   }
   //alert("max dlina mass proizv   "+newmass.length);
-  var mult_elem=0;var e=0;var k=newmass.length-1;var y=0;
-  var p=0;//для сдвига при умножении
+
+  var mult_elem=0;
+  var k;
   for(var i=short_str.length-1;i>=0;i--)// берем элементы из короткой строки
-  { 
+  {     
     for(var j=long_str.length-1;j>=0;j--)//для каждого элемента длинной 
                                       //строки перебираем элементы короткой строки
-    { 
-     //alert("korotki*dlinn: "+short_str.charAt(i)+" * "+long_str.charAt(j));
+    {    k=i+j+1;  
+    // alert("korotki*dlinn: "+short_str.charAt(i)+" * "+long_str.charAt(j));
      mult_elem=(+(short_str.charAt(i)))*(+(long_str.charAt(j)));
-     //alert("rez mult "+mult_elem);
-          //if(mult_elem==0){e++;}alert("mult_elem "+mult_elem);
-          if(mult_elem<10)
-          {
-          newmass[k+y]=newmass[k+y]+mult_elem;
-          }else{
-          newmass[k+y]=newmass[k]+(mult_elem%10);
-          newmass[k-1+y]=newmass[k-1+y]+((mult_elem-(mult_elem%10))/10);
-          }
-    //alert("k-e elementi  ["+"k="+k+"  y= "+ y+"]"+newmass[k+y]);   
+   // alert("rez mult "+mult_elem);
+    if(newmass[k]=='*')//если первый раз зашли
+      {
+        newmass[k]=0; 
+        if(mult_elem<10)//если элемент <10
+        {
+        newmass[k]=newmass[k]+mult_elem; alert("prisv mult_elem "+mult_elem+ " newmass[ "+k);
 
-    // alert("mult_elem/10= "+((mult_elem-(mult_elem%10))/10));
-     //alert("ostatok "+mult_elem%10);
-     k--;    
-    }y++;k++;
-         
-//alert("newmass["+i+"]= "+newmass[i]);
+        }else{//если элемент>9 и это первый заход
+          //alert("prisv mult_elem "+(mult_elem%10)+ " newmass[ "+k);
+          newmass[k]=newmass[k]+(mult_elem%10);//второе число даем текущему эл-ту
+          newmass[k-1]=0;
+          //alert("prisv mult_elem "+((mult_elem-(mult_elem%10))/10)+ " newmass[ "+(k-1));
+          newmass[k-1]=newmass[k-1]+((mult_elem-(mult_elem%10))/10);//первое отдаем предыдущему эл-ту
+
+
+             }
+      } else{//если в newmass уже есть значение
+             if(mult_elem<10)
+            {
+              newmass[k]=newmass[k]+mult_elem;
+              //alert("prisv mult_elem "+mult_elem+ " newmass[ "+k);
+
+                if(newmass[k]>9)
+                  {
+                   //alert("izmenaem "+newmass[k]+ " na "+(newmass[k]%10)+"newmass[ "+k);
+                   newmass[k]=newmass[k]%10;
+                   if(newmass[k-1]=='*')
+                     {
+                       newmass[k-1]=0;
+                       newmass[k-1]=newmass[k-1]+1;
+                       //alert("prisv +1 k newmass[ "+(k-1));
+                     } else{
+                      //alert("prisv +1 k newmass[ "+(k-1));
+                      newmass[k-1]=newmass[k-1]+1;}
+                  }
+            } else {//если в newmass уже есть значение и mult_elem>9
+                    //alert("prisv mult_elem "+(mult_elem%10)+ " newmass[ "+k);
+                    newmass[k]=newmass[k]+(mult_elem%10);//второе число даем текущему эл-ту
+                    if(newmass[k-1]=='*')
+                    {
+                      newmass[k-1]=0;
+                      //alert("prisv mult_elem "+((mult_elem-(mult_elem%10))/10)+ " newmass[ "+(k-1));
+                      newmass[k-1]=newmass[k-1]+((mult_elem-mult_elem%10)/10);//первое отдаем предыдущему эл-ту?????*
+                    } else{
+                      //alert("prisv mult_elem "+((mult_elem-(mult_elem%10))/10)+ " newmass[ "+(k-1));
+                      newmass[k-1]=newmass[k-1]+((mult_elem-mult_elem%10)/10);}
+                   }
+      }
+
+    }
+
+
 
   }
-  for(var i=0;i<(long_str.length+short_str.length);i++)
+//alert("newmass 1= "+newmass);
+     for(var g=newmass.length;g>0;g--)
+     {
+         //alert("g=  "+g)
+         //alert("newmass[g]=  "+newmass[g]);
+              if(newmass[g]>9)//////////////////////////////////////////??????
+                {
+                 // alert("poluholi "+newmass[g]+" s nomerom "+g);
+                  var h1=newmass[g]; 
+                  newmass[g]=(newmass[g]%10); 
+                  var h=newmass[g-1];                                   
+                  newmass[g-1]=newmass[g-1]+((h1-h1%10)/10);
+                      
+
+                }///////////////////////////////////////////////////
+          
+     }
+
+  //alert("newmass 2= "+newmass);
+
+//alert("&&&&&&&&&&&&&&&&   "+newmass);
+
+
+
+  for(var i=0;i<(newmass.length);i++)
   {
     if(newmass[i]==0)
     {
@@ -54,11 +115,13 @@ module.exports =function multiply(first, second) {
     }
     else{break;}
   }
-  var mult_str=newmass.join('');//переводим массив в строку
-  mult_str=mult_str.replace('*','');
 
-  //alert("poluhilas stroka: "+mult_str);
+  var mult_str=newmass.join('');//переводим массив в строку
+   for(var i=0;i<(newmass.length);i++)
+  {
+  mult_str=mult_str.replace('*','');
+}
+ // alert("poluhilas stroka: "+mult_str);
 
   return mult_str;
 }
-
