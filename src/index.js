c60095cd8cbe4d0c2584f9a8fc47a8afd9cdfec4
Helen Trapeznikova <31996958+HelenTR7@users.noreply.github.com>
2017-09-28 19:22:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-module.exports = function multiply(first, second) {
+module.exports =function multiply(first, second) {
+
   var long_str, short_str;
-  if(first.length>second.length)
+  if(first.length>=second.length)
   {
    long_str=first;
    short_str=second;
@@ -12,51 +13,51 @@ module.exports = function multiply(first, second) {
   }
 
   var newmass=[];
-  for(i=0;i<long_str.length+1;i++)//заполняем нулями
+  for(i=0;i<(long_str.length+short_str.length);i++)//заполняем нулями
   {
     newmass[i]=0;
   }
-  var mult_elem=0;
+ // alert("max dlina mass proizv   "+newmass.length);
+  var mult_elem=0;var e=0;var k=newmass.length-1;var y=0;
   var p=0;//для сдвига при умножении
   for(var i=short_str.length-1;i>=0;i--)// берем элементы из короткой строки
-  {
+  { 
     for(var j=long_str.length-1;j>=0;j--)//для каждого элемента длинной 
                                       //строки перебираем элементы короткой строки
     { 
      //alert("korotki*dlinn: "+short_str.charAt(i)+" * "+long_str.charAt(j));
      mult_elem=(+(short_str.charAt(i)))*(+(long_str.charAt(j)));
      //alert("rez mult "+mult_elem);
-     newmass[j+p]=newmass[j+p]+(mult_elem%10);
-     newmass[j-1+p]=newmass[j-1+p]+((mult_elem-(mult_elem%10))/10);
-     if(newmass[j-1+p]>9)
-     {
-      newmass[j-2+p]=newmass[j-2+p]+1;
-      newmass[j-1+p]=newmass[j-1+p]%10;
-     }
+          //if(mult_elem==0){e++;}alert("mult_elem "+mult_elem);
+          if(mult_elem<10)
+          {
+          newmass[k+y]=newmass[k+y]+mult_elem;
+          }else{
+          newmass[k+y]=newmass[k]+(mult_elem%10);
+          newmass[k-1+y]=newmass[k-1+y]+((mult_elem-(mult_elem%10))/10);
+          }
+    //alert("k-e elementi  ["+(k+y)+"]"+newmass[k+y]);   
+
     // alert("mult_elem/10= "+((mult_elem-(mult_elem%10))/10));
      //alert("ostatok "+mult_elem%10);
-     
-     
-    }
-    p++;
+     k--;    
+    }y++;
+         
 //alert("newmass["+i+"]= "+newmass[i]);
 
   }
-  //alert("massiv  "+newmass);
-  for(var i=0; i<newmass.length;i++)
-  { //alert("'element newmass ["+i+"] = "+newmass[i]);
-       if(newmass[i]>9)
-     {
-      newmass[i-1]=newmass[i-1]+1;
-      newmass[i]=newmass[i]%10;
-     }
+  for(var i=0;i<(long_str.length+short_str.length);i++)
+  {
+    if(newmass[i]==0)
+    {
+      newmass[i]='*';
+    }
+    else{break;}
   }
-  //alert("massiv  "+newmass);
-
-
   var mult_str=newmass.join('');//переводим массив в строку
+  mult_str=mult_str.replace('*','');
+
   //alert("poluhilas stroka: "+mult_str);
 
   return mult_str;
-
 }
